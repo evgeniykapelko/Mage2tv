cff9558203c3fc31ccf8ae3c566dda1fbafe97fa
Evgeniy <ekapelko@corp.web4pro.com.ua>
2018-10-13 21:24:48 +0300

    widget the end but nedd learning

---
+++ Js/view/frontend/web/js/plain-view-model.js
@@ -0,0 +1,9 @@
+define([], function () {
+    'use strict';
+
+    return function () {
+        return {
+            title: 'This is a very fine title for a simple but good view model!'
+        }
+    }
+});
